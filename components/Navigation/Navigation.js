7ef9a5c3da9bf7a8f20ad67c2f1b8faaba685a5b
Charis Amend <charis.amend@googlemail.com>
2024-02-05 17:57:21 +0100

    added the tests

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -6,15 +6,15 @@ export default function Navigation() {
         <>
             <StyledNav className="navbar">
 
-                <StyledNavbarItem className="navbar-item">
+                <button className="navbar-item">
                     <Link href="../../pages/index.js">Spotlight</Link>
-                </StyledNavbarItem>
-                <StyledNavbarItem className="navbar-item">
+                </button>
+                <button className="navbar-item">
                     <Link href="../../pages/art-pieces-page/">Pieces</Link>
-                </StyledNavbarItem>
-                <StyledNavbarItem className="navbar-item">
+                </button>
+                <button className="navbar-item">
                     <Link href="../../pages/favorites-page/">Favorites</Link>
-                </StyledNavbarItem>
+                </button>
 
             </StyledNav>
         </>
@@ -36,7 +36,7 @@ const StyledNav = styled.nav`
     justify-content: space-between;
   `
 
-const StyledNavbarItem = styled.p`
+const StyledNavbarItem = styled.button`
         background-color: white;
         box-sizing: border-box;
 margin: 5px;
