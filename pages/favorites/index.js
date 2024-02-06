6c0dd169ca6dc6a1c3df890650f115cd62d021b1
Charis Amend <charis.amend@googlemail.com>
2024-02-06 18:00:49 +0100

    deleted mockup from favorites/index.js

--- pages/favorites/index.js
+++ pages/favorites/index.js
@@ -2,17 +2,17 @@ import ArtPieces from "@/components/ArtPieces/ArtPieces";
 import React from "react";
 
 //mockup:
-const artPiecesInfo = [
-  {
-    slug: "orange-red-and-green",
-    isFavorite: true,
-  },
-  { slug: "blue-and-red", isFavorite: false },
-];
+// const artPiecesInfo = [
+//   {
+//     slug: "orange-red-and-green",
+//     isFavorite: true,
+//   },
+//   { slug: "blue-and-red", isFavorite: false },
+// ];
 
 // artPiecesInfo needs to be passed a prop, mockup deleted
 
-export default function Favorites({ pieces }) {
+export default function Favorites({ pieces, artPiecesInfo }) {
   const filteredFavorites = artPiecesInfo.filter((e) => e.isFavorite === true);
   const filteredSlugs = filteredFavorites.map((e) => e.slug);
   const filteredPieces = pieces.filter((e) => filteredSlugs.includes(e.slug));
